bae161b9e87ae3628c9481c54d0e810d9879ca51
shellynj <mb884@njit.edu>
2019-11-01 19:13:18 -0400

    Feature: Added addition and CsvReader definitions

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,32 @@
+import math
+from CsvReader import CsvReader
+
+
+
+def addition(a, b):
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
 
 
 class Calculator:
-    result = 0
+        result = 0
 
-    def __init__(self):
+def __init__(self):
+            pass
+
+
+def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+
+
+
+class CSVStats(Calculator):
+    data = []
+
+    def __init__(self, data_file):
+        self.data = CsvReader(data_file)
         pass
-
-
